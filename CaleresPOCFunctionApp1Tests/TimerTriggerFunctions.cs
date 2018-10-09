da47864c692017bae710eacf6fc03686e9e2d0ef
Marcus Brandel <mbrandel@rbaconsulting.com>
2018-10-09 11:44:18 -0500

    Added test project.

---
+++ CaleresPOCFunctionApp1Tests/TimerTriggerFunctions.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaleresPOCFunctionApp1Tests
+{
+    [TestClass]
+    public class TimerTriggerFunctions
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
